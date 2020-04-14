cfa53326d4288d565883b3995d02d83643395f06
Natalia Shport <shport.natalia@gmail.com>
2020-04-14 00:25:07 -0700

    Updates

---
+++ precache-manifest.eb54c9e00c580acd88f01adc5487e50a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a9688c457209a62433cace94aaa3bac",
+    "url": "/pets-rolodex/index.html"
+  },
+  {
+    "revision": "04f36b7e016d2adbb1a3",
+    "url": "/pets-rolodex/static/css/main.4ed9bb86.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/pets-rolodex/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pets-rolodex/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04f36b7e016d2adbb1a3",
+    "url": "/pets-rolodex/static/js/main.d85f2bcb.chunk.js"
+  },
+  {
+    "revision": "28ba4498f3585e1ef23e",
+    "url": "/pets-rolodex/static/js/runtime-main.dc22c584.js"
+  }
+]);
